7f7fd1acd6728ebe0ed89daeefbd17a941fd4fd3
EGOR KIRYUKHIN <true.g1ff@gmail.com>
2025-02-02 23:29:59 +0700

    implement Calculator

---
+++ Lession_1/Calculator.java
@@ -0,0 +1,45 @@
+public class Calculator { 
+    public static void main(String[] args) {
+        int a = 201;
+        int b = 10;
+        int result = 0;
+        char mathOperator = '%';
+
+        if (a < 0 && b < 0) {
+            System.out.println("Введено неверное значение чисел А и В");
+        } else if (a < 0) {
+            System.out.println("Введено неверное значение числа А");
+        } else if (b < 0) {
+            System.out.println("Введено неверное значение числа В");
+        }
+
+        switch (mathOperator) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+            case '^':
+                int i = 1;
+                result = a;
+                while (i < b) {
+                    result *= a; 
+                    i++;
+                }
+                break;
+            case '%':
+                result = a % b;
+                break;
+            default:
+                System.out.println("Введен неверный математический оператор!");
+        }
+        System.out.println(a + " " + mathOperator + " " + b + " = " + result);
+    }
+}
