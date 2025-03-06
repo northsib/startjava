e75cfcaef40efd620f55be37bfd23544d64199d8
EGOR KIRYUKHIN <true.g1ff@gmail.com>
2025-03-06 23:09:51 +0700

    delete old directories

--- Startjava_project/src/com/startjava/lession1/base/MyFirstApp.java
+++
@@ -1,7 +0,0 @@
-package com.startjava.lession1.base;
-
-public class MyFirstApp {
-    public static void main(String[] args) {
-        System.out.println("Write once, run anywhere!");
-    }
-}
--- Startjava_project/src/com/startjava/lession2/method/naming/MethodName.java
+++
@@ -1,7 +0,0 @@
-package com.startjava.lession2.method.naming;
-
-public class MethodName {
-    public static String getCurrent() {
-        return Thread.currentThread().getStackTrace()[2].getMethodName();
-    }
-}
