786c81acf8d284d586b5cde597a04c94b0eaf310
EGOR KIRYUKHIN <true.g1ff@gmail.com>
2025-03-13 21:23:59 +0700

    modified ArrayReverser, delete others

--- src/com/startjava/lession2_3_4/array/ArrayReverser.java
+++ src/com/startjava/lession2_3_4/array/ArrayReverser.java
@@ -1,60 +1,50 @@
 package com.startjava.lession2_3_4.array;
 
-import java.util.Scanner;
-
 public class ArrayReverser {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите длину массива: ");
-        int arrayLength = scanner.nextInt();
-        int[] numbers = createArray(scanner, arrayLength);
-        if (isCorrectArray(numbers)) {
-            printArray(numbers);
-            reverseArray(numbers);
-        }
-    }
-
-    public static int[] createArray(Scanner scanner, int arrayLength) {
-        if (arrayLength == 0) {
-            System.out.println("Массив нулевой длины");
-            return null;
-        }
-        int[] numbers = new int[arrayLength];
-        System.out.print("заполните массив целыми числами через пробел: ");
-        for (int i = 0; i < arrayLength; i++) {
-            numbers[i] = scanner.nextInt();
-        }
-        return numbers;
-    }
-
-    public static boolean isCorrectArray(int[] numbers) {
-        if (numbers == null || numbers.length == 0) {
-            System.out.println("null");
-            return false;
-        }
-        return true;
-    }
-
-    public static void printArray(int[] numbers) {
+        int[] emptyArray = {};
+        int[] nullArray = null;
+        int[] array4 = {6, 8 ,9, 1};
+        int[] array7 = {13, 8, 5, 3, 2, 1, 1};
+        printArray(emptyArray);
+        printArray(nullArray);
         System.out.printf("%16s", "До реверса: [");
+        printArray(array4);
+        System.out.print("]\n");
+        int[] reversed = reverseArray(array4);
+        System.out.print("После реверса: [");
+        printArray(reversed);
+        System.out.printf("%s%n%16s", "]", "До реверса: [");
+        printArray(array7);
+        System.out.print("]\n");
+        reversed = reverseArray(array7);
+        System.out.print("После реверса: [");
+        printArray(reversed);
+        System.out.print("]\n");
+    }
+
+    private static void printArray(int[] numbers) {
+        if (numbers == null) {
+            System.out.println("null");
+            return;
+        }
+        if (numbers.length == 0) {
+            System.out.println("Массив нулевой длины");
+            return;
+        }
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i]);
             if (i < (numbers.length - 1)) {
                 System.out.print(", ");
             }
         }
-        System.out.println("]");
     }
 
-    public static void reverseArray(int[] numbers) {
-        System.out.print("После реверса: [");
-        for (int i = (numbers.length - 1); i >= 0; i--) {
-            System.out.print(numbers[i]);
-            if (i > 0) {
-                System.out.print(", ");
-            }
+    private static int[] reverseArray(int[] numbers) {
+        int[] reversed = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            reversed[i] = numbers[(numbers.length - 1) - i];
         }
-        System.out.println("]");
+        return reversed;
     }
 }
