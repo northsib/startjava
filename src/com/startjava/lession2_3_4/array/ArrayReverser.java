d0f95136d2a82eeca2d72246f64d1a1af71f264e
Egor Kiryukhin <egor.kiryukhin95@gmail.com>
2025-03-15 09:47:38 +0700

    modified ArrayReverser

--- src/com/startjava/lession2_3_4/array/ArrayReverser.java
+++ src/com/startjava/lession2_3_4/array/ArrayReverser.java
@@ -1,49 +1,50 @@
 package com.startjava.lession2_3_4.array;
 
+import java.util.Arrays;
+
 public class ArrayReverser {
     public static void main(String[] args) {
         int[] emptyArray = {};
+        int[] reverseEmpty = reverse(emptyArray);
         int[] nullArray = null;
+        int[] reverseNull = reverse(nullArray);
         int[] array4 = {6, 8 ,9, 1};
+        int[] reverse4 = reverse(array4);
         int[] array7 = {13, 8, 5, 3, 2, 1, 1};
-        printArray(emptyArray);
-        printArray(nullArray);
-        System.out.printf("%16s", "До реверса: [");
-        printArray(array4);
-        System.out.print("]\n");
-        int[] reversed = reverseArray(array4);
-        System.out.print("После реверса: [");
-        printArray(reversed);
-        System.out.printf("%s%n%16s", "]", "До реверса: [");
-        printArray(array7);
-        System.out.print("]\n");
-        reversed = reverseArray(array7);
-        System.out.print("После реверса: [");
-        printArray(reversed);
-        System.out.print("]\n");
+        int[] reverse7 = reverse(array7);
+
+        System.out.println("Вызовем пустой массив, выполним его реверс и выведем результат на экран: ");
+        System.out.printf("%18s", "До реверса: " + Arrays.toString(emptyArray)+ "\n");
+        System.out.println("После реверса: " + Arrays.toString(reverseEmpty));
+
+        System.out.println("\nВызовем массив \"null\", выполним его реверс и выведем результат на экран: ");
+        System.out.printf("%20s", "До реверса: " + Arrays.toString(nullArray)+ "\n");
+        System.out.println("После реверса: " + Arrays.toString(reverseNull));
+
+        System.out.println("\nВызовем массив из 4 заданных элементов, " +
+                "выполним его реверс и выведем результат на экран: ");
+        System.out.printf("%28s", "До реверса: " + Arrays.toString(array4) + "\n");
+        System.out.print("После реверса: " + Arrays.toString(reverse4) + "\n");
+
+        System.out.println("\nВызовем массив из 7 заданных элементов, " +
+                "выполним его реверс и выведем результат на экран: ");
+        System.out.printf("%38s", "До реверса: " + Arrays.toString(array7) + "\n");
+        System.out.print("После реверса: " + Arrays.toString(reverse7) + "\n");
     }
 
-    private static void printArray(int[] numbers) {
+    private static int[] reverse(int[] numbers) {
         if (numbers == null) {
-            System.out.println("null");
-            return;
+            return null;
         }
         if (numbers.length == 0) {
-            System.out.println("Массив нулевой длины");
-            return;
+            return numbers;
         }
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i]);
-            if (i < (numbers.length - 1)) {
-                System.out.print(", ");
-            }
-        }
-    }
 
-    private static int[] reverseArray(int[] numbers) {
         int[] reversed = new int[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            reversed[i] = numbers[(numbers.length - 1) - i];
+        int index = 0;
+        for (int number : numbers) {
+            reversed[numbers.length - 1 - index] = number;
+            index++;
         }
         return reversed;
     }
