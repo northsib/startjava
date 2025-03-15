d445c50d7e2e987d6f764bf8404d916c117f4cba
Egor Kiryukhin <egor.kiryukhin95@gmail.com>
2025-03-15 14:50:16 +0700

    modified Factorial

--- src/com/startjava/lession2_3_4/array/Factorial.java
+++ src/com/startjava/lession2_3_4/array/Factorial.java
@@ -2,14 +2,36 @@ package com.startjava.lession2_3_4.array;
 
 public class Factorial {
     public static void main(String[] args) {
-        calculateFactorial();
-        calculateFactorial(null);
-        calculateFactorial(8, 0, 9);
-        calculateFactorial(-3, 1, 7, 13);
-        calculateFactorial(-22, -0);
+        print(calculate());
+        print(calculate(null), null);
+        print(calculate(8, 0, 9), 8, 0, 9);
+        print(calculate(-3, 1, 7, 13), -3, 1, 7, 13);
+        print(calculate(-22, -0), -22, -0);
     }
 
-    private static void calculateFactorial (long... numbers) {
+    private static long[] calculate(long... numbers) {
+        if (numbers == null) {
+            return null;
+        }
+        if (numbers.length == 0) {
+            return numbers;
+        }
+        long[] factorials = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < 0) {
+                factorials[i] = -1000;
+                continue;
+            }
+            long factorial = 1;
+            for (int j = 1; j <= numbers[i]; j++) {
+                factorial *= j;
+            }
+            factorials[i] = factorial;
+        }
+        return factorials;
+    }
+
+    private static void print(long[] factorials, int... numbers) {
         if (numbers == null) {
             System.out.println("null\n");
             return;
@@ -19,32 +41,20 @@ public class Factorial {
             return;
         }
 
-        long[] factorials = new long[numbers.length];
-        String[] factorialExpression = new String[numbers.length];
-
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] < 0) {
-                factorials[i] = -1;
+            if (factorials[i] == 1) {
+                System.out.println(numbers[i] + "! = 1");
                 continue;
             }
-            long factorial = 1;
-            String expression = "";
+            if (factorials[i] == -1000) {
+                System.out.println("Ошибка: факториал " + numbers[i] + "! не определен");
+                continue;
+            }
+            StringBuilder factorialExpression = new StringBuilder();
             for (int j = 1; j <= numbers[i]; j++) {
-                factorial *= j;
-                expression += (j > 1 ? " * " : "") + j;
+                factorialExpression.append(j > 1 ? " * " + j : j);
             }
-            factorials[i] = factorial;
-            factorialExpression[i] = expression;
-        }
-
-        for (int i = 0; i < factorials.length; i++) {
-            if (factorials[i] == 1) {
-                System.out.printf(numbers[i] + "! = 1\n");
-            } else {
-                System.out.println((factorials[i] == -1 ?
-                        "Ошибка: факториал " + numbers[i] + "! не определен" :
-                        numbers[i] + "! = " + factorialExpression[i] + " = " + factorials[i]));
-            }
+            System.out.println(numbers[i] + "! = " + factorialExpression + " = " + factorials[i]);
         }
         System.out.println();
     }
