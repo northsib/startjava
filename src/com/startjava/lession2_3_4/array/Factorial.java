309a5f7a0539c5a81160ba2e9909db29c97a2ba3
EGOR KIRYUKHIN <true.g1ff@gmail.com>
2025-03-16 22:10:17 +0700

    modified Factorial - 3

--- src/com/startjava/lession2_3_4/array/Factorial.java
+++ src/com/startjava/lession2_3_4/array/Factorial.java
@@ -2,38 +2,34 @@ package com.startjava.lession2_3_4.array;
 
 public class Factorial {
     public static void main(String[] args) {
-        long[] factorials = calculate();
-        int[] original = {};
-        print(original, factorials);
+        int[] emptyOriginal = {};
+        long[] factorials = calculate(emptyOriginal);
+        print(emptyOriginal, factorials);
 
-        factorials = calculate(null);
-        original = null;
-        print(original, factorials);
+        int[] nullOriginal = null;
+        factorials = calculate(nullOriginal);
+        print(nullOriginal, factorials);
 
-        factorials = calculate(8, 0, 9);
-        original = new int[]{8, 0, 9};
-        print(original, factorials);
+        int[] original3 = {8, 0, 9};
+        factorials = calculate(original3);
+        print(original3, factorials);
 
-        factorials = calculate(-3, 1, 7, 13);
-        original = new int[]{-3, 1, 7, 13};
-        print(original, factorials);
+        int[] original4 = {-3, 1, 7, 13};
+        factorials = calculate(original4);
+        print(original4, factorials);
 
-        factorials = calculate(-22, -0);
-        original = new int[]{-22, -0};
-        print(original, factorials);
+        int[] original2 = {-22, -0};
+        factorials = calculate(original2);
+        print(original2, factorials);
     }
 
     private static long[] calculate(int... numbers) {
-        if (numbers == null) {
-            return null;
-        }
-        if (numbers.length == 0) {
+        if (numbers == null || numbers.length == 0) {
             return null;
         }
         long[] factorials = new long[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] < 0) {
-                factorials[i] = 0;
                 continue;
             }
             long factorial = 1;
@@ -68,10 +64,7 @@ public class Factorial {
             for (int j = 1; j <= original[i]; j++) {
                 factorialExpression.append(j > 1 ? " * " + j : j);
             }
-            StringBuilder outputExpression = new StringBuilder();
-            System.out.println(outputExpression.append(original[i])
-                    .append("! = ").append(factorialExpression)
-                    .append(" = ").append(factorials[i]));
+            System.out.println(original[i] + "! = " + factorialExpression + " = " + factorials[i]);
         }
         System.out.println();
     }
