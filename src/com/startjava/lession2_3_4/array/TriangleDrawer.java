7a64242debba640c9e2c183d35f6bf21718b3dd6
Egor Kiryukhin <egor.kiryukhin95@gmail.com>
2025-03-17 11:15:10 +0700

    modified TriangleDrawer - 2

--- src/com/startjava/lession2_3_4/array/TriangleDrawer.java
+++ src/com/startjava/lession2_3_4/array/TriangleDrawer.java
@@ -2,28 +2,37 @@ package com.startjava.lession2_3_4.array;
 
 public class TriangleDrawer {
     public static void main(String[] args) {
-        print('0', '9', true);
-        print('/', '!', false);
-        print('A', 'J', false);
+        String[] triangle = create('0', '9', true);
+        print1(triangle);
+        triangle = create('/', '!', false);
+        print1(triangle);
+        triangle = create('A', 'J', true);
+        print1(triangle);
     }
 
-    private static void print(char start, char end, boolean ascending) {
+    private static String[] create(char start, char end, boolean ascending) {
         if (start > end) {
-            System.out.println("Ошибка: левая граница (" + (int) start + ") > правой (" + (int) end + ")\n");
-            return;
+            return new String[]{"\nОшибка: левая граница (" + start + ") > (" + end + ")\n"};
         }
-        int height = end - (start - 1);
-        char startPrint = ascending ? end : start;
 
-        for (int i = 0; i < height; i++) {
+        int triangleHeight = end - start + 1;
+        char startPrint = ascending ? end : start;
+        String[] triangle = new String[triangleHeight];
+        StringBuilder creator = new StringBuilder();
+        for (int i = 0; i < triangleHeight; i++) {
             char currentChar = (char) (ascending ? startPrint - i : startPrint + i);
-            StringBuilder spaces = new StringBuilder();
-            spaces.append(" ".repeat(Math.max(0, (height - 1) - i)));
-            StringBuilder chars = new StringBuilder();
-            chars.append(String.valueOf(currentChar).repeat(2 * i + 1));
-            System.out.println(spaces + chars.toString());
+            creator.append(" ".repeat(Math.max(0, (triangleHeight - 1) - i)))
+                        .append(String.valueOf(currentChar).repeat(2 * i + 1));
+            triangle[i] = String.valueOf(creator);
+            creator = new StringBuilder();
         }
-        System.out.println();
+        return triangle;
+    }
+
+    private static void print1(String[] triangle) {
+        for (String s : triangle) {
+            System.out.println(s);
+        }
     }
 }
 
