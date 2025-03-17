db55d4a29759e008f01ac4e943bbe5db3e042e61
EGOR KIRYUKHIN <true.g1ff@gmail.com>
2025-03-17 21:45:02 +0700

    modified TriangleDrawer - 4

--- src/com/startjava/lession2_3_4/array/TriangleDrawer.java
+++ src/com/startjava/lession2_3_4/array/TriangleDrawer.java
@@ -2,33 +2,28 @@ package com.startjava.lession2_3_4.array;
 
 public class TriangleDrawer {
     public static void main(String[] args) {
-        StringBuilder triangle = create('0', '9', true);
-        draw(triangle);
-
-        triangle = create('/', '!', false);
-        draw(triangle);
-
-        triangle = create('A', 'J', false);
-        draw(triangle);
+        draw(create('0', '9', true));
+        draw(create('/', '!', false));
+        draw(create('A', 'J', false));
     }
 
     private static StringBuilder create(char start, char end, boolean ascending) {
         if (start > end) {
-            System.out.println("\nОшибка: левая граница (" + start + ") > (" + end + ")\n");
+            System.out.println("Ошибка: левая граница (" + start + ") > (" + end + ")\n");
             return null;
         }
 
         int triangleHeight = end - start + 1;
         char startPrint = ascending ? end : start;
 
-        StringBuilder creator = new StringBuilder();
+        StringBuilder triangle = new StringBuilder();
         for (int i = 0; i < triangleHeight; i++) {
             char currentChar = (char) (ascending ? startPrint - i : startPrint + i);
-            creator.append(" ".repeat(Math.max(0, (triangleHeight - 1) - i)))
+            triangle.append(" ".repeat(Math.max(0, (triangleHeight - 1) - i)))
                     .append(String.valueOf(currentChar).repeat(2 * i + 1))
                     .append("\n");
         }
-        return creator;
+        return triangle;
     }
 
     private static void draw(StringBuilder triangle) {
