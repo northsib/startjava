99ebc41233dc6cf885d7ea842dd922fee808433c
Egor Kiryukhin <egor.kiryukhin95@gmail.com>
2025-03-17 16:44:04 +0700

    modified TriangleDrawer - 3

--- src/com/startjava/lession2_3_4/array/TriangleDrawer.java
+++ src/com/startjava/lession2_3_4/array/TriangleDrawer.java
@@ -2,38 +2,39 @@ package com.startjava.lession2_3_4.array;
 
 public class TriangleDrawer {
     public static void main(String[] args) {
-        String[] triangle = create('0', '9', true);
-        print1(triangle);
+        StringBuilder triangle = create('0', '9', true);
+        draw(triangle);
+
         triangle = create('/', '!', false);
-        print1(triangle);
-        triangle = create('A', 'J', true);
-        print1(triangle);
+        draw(triangle);
+
+        triangle = create('A', 'J', false);
+        draw(triangle);
     }
 
-    private static String[] create(char start, char end, boolean ascending) {
+    private static StringBuilder create(char start, char end, boolean ascending) {
         if (start > end) {
-            return new String[]{"\nОшибка: левая граница (" + start + ") > (" + end + ")\n"};
+            System.out.println("\nОшибка: левая граница (" + start + ") > (" + end + ")\n");
+            return null;
         }
 
         int triangleHeight = end - start + 1;
         char startPrint = ascending ? end : start;
-        String[] triangle = new String[triangleHeight];
+
         StringBuilder creator = new StringBuilder();
         for (int i = 0; i < triangleHeight; i++) {
             char currentChar = (char) (ascending ? startPrint - i : startPrint + i);
             creator.append(" ".repeat(Math.max(0, (triangleHeight - 1) - i)))
-                        .append(String.valueOf(currentChar).repeat(2 * i + 1));
-            triangle[i] = String.valueOf(creator);
-            creator = new StringBuilder();
+                    .append(String.valueOf(currentChar).repeat(2 * i + 1))
+                    .append("\n");
         }
-        return triangle;
+        return creator;
     }
 
-    private static void print1(String[] triangle) {
-        for (String s : triangle) {
-            System.out.println(s);
+    private static void draw(StringBuilder triangle) {
+        if (triangle == null) {
+            return;
         }
+        System.out.println(triangle);
     }
 }
-
-
