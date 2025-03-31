8f1a4b68550d54789337ae9648f2eaffe1527bbc
EGOR KIRYUKHIN <true.g1ff@gmail.com>
2025-03-31 15:33:27 +0700

    working calculator 4.5

--- src/com/startjava/lession2_3_4/calculator/Calculator.java
+++ src/com/startjava/lession2_3_4/calculator/Calculator.java
@@ -1,72 +1,147 @@
 package com.startjava.lession2_3_4.calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
-    private int firstNumber;
-    private int secondNumber;
-    private char mathOperator;
-    private int result;
+    private static final String[] MATH_OPERATORS = {"+", "-", "/", "*", "%", "^"};
+    private static final int ARGUMENTS_COUNT = 3;
 
-    public void setFirstNumber(int firstNumber) {
-        this.firstNumber = firstNumber;
+    private Calculator() {
     }
 
-    public void setSecondNumber(int secondNumber) {
-        this.secondNumber = secondNumber;
-    }
+    public static void calculate(String expression) {
+        String[] correctExpression = makeCorrectExpression(expression);
+        double result;
 
-    public boolean setMathOperator(char mathOperator) {
-        switch (mathOperator) {
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-            case '^':
-            case '%':
-                this.mathOperator = mathOperator;
-                return true;
-            default:
-                System.out.println("Ошибка: операция '" + mathOperator + "' не поддерживается");
-                return false;
-        }
-    }
-
-    public void calculate() {
-        float floatResult = 0;
-        switch (mathOperator) {
-            case '+':
-                result = firstNumber + secondNumber;
-                break;
-            case '-':
-                result = firstNumber - secondNumber;
-                break;
-            case '*':
-                result = firstNumber * secondNumber;
-                break;
-            case '/':
-                if (secondNumber == 0) {
-                    System.out.println("Ошибка: делить на ноль нельзя");
-                    return;
-                }
-                result = firstNumber / secondNumber;
-                floatResult = (firstNumber < secondNumber || (firstNumber % secondNumber) > 0) ? 
-                        (float) firstNumber / secondNumber : 0;
-                break;
-            case '^':
-                int absoluteSecondNumber = Math.abs(secondNumber);
-                result = 1;
-                for (int i = 1; i <= absoluteSecondNumber; i++) {
-                    result *= firstNumber;
-                }
-                floatResult = secondNumber < 0 ? 1 / (float) result : 0;
-                break;
-            case '%':
-                result = firstNumber % secondNumber;
-                break;
-        }
-        if (floatResult != 0) {
-            System.out.println("Ответ: " + floatResult);
+        if (correctExpression == null || correctExpression.length != 3) {
+            result = Double.NaN;
         } else {
-            System.out.println("Ответ: " + result);
+            try {
+                int firstNumber = Integer.parseInt(correctExpression[0]);
+                int secondNumber = Integer.parseInt(correctExpression[2]);
+                char mathOperator = correctExpression[1].charAt(0);
+                result = switch (mathOperator) {
+                    case '+' -> firstNumber + secondNumber;
+                    case '-' -> firstNumber - secondNumber;
+                    case '*' -> firstNumber * secondNumber;
+                    case '/' -> {
+                        if (secondNumber == 0) {
+                            displayError("На ноль делить нельзя!");
+                            yield Double.NaN;
+                        }
+                        yield (double) firstNumber / secondNumber;
+                    }
+                    case '^' -> Math.pow(firstNumber, secondNumber);
+                    case '%' -> (double) Math.floorMod(firstNumber, secondNumber);
+                    default -> Double.NaN;
+                };
+            } catch (NumberFormatException e) {
+                displayError("Некорректно введено одно из чисел " + "(введено: \"" +
+                        correctExpression[0] + "\" " +
+                        "\"" + correctExpression[2] + "\")\nДопустимо: целые положительные, " +
+                        "отрицательные числа, либо 0 (в диапазоне значений int)");
+                result = Double.NaN;
+            }
         }
+        printResult(correctExpression, result);
+    }
+
+    private static String[] makeCorrectExpression(String expression) {
+        String[] splits = splitExpression(expression);
+        if (splits == null || splits.length == 0) {
+            displayError("Введено пустое выражение");
+            return null;
+        }
+        String[] correctExpression = new String[ARGUMENTS_COUNT];
+        int[] mathOperatorIndexes = parseMathOperatorIndex(splits);
+        if (mathOperatorIndexes == null) {
+            displayError("Не введены мат операторы");
+            return null;
+        }
+        correctExpression[0] = parseFirstNumber(splits, mathOperatorIndexes[0]);
+        correctExpression[1] = splits[mathOperatorIndexes[0]];
+        correctExpression[2] = parseSecondNumber(splits, mathOperatorIndexes);
+        if (correctExpression[2] == null) {
+            displayError("Некорректный ввод мат операторов, введено: \"" + splits[mathOperatorIndexes[0]] +
+                    splits[mathOperatorIndexes[1]] + "\", допустимо: \"" +
+                    splits[mathOperatorIndexes[0]] + "\"");
+            return null;
+        }
+        return correctExpression;
+    }
+
+    private static String[] splitExpression(String expression) {
+        String[] temp = expression.split("");
+        int index = 0;
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i].isBlank()) {
+                continue;
+            }
+            temp[index] = temp[i];
+            index++;
+        }
+        return Arrays.copyOf(temp, index);
+    }
+
+    private static int[] parseMathOperatorIndex(String[] splits) {
+        int index1 = -1;
+        int index2 = -1;
+
+        for (int i = 0; i < splits.length; i++) {
+            for (String mathOperator : MATH_OPERATORS) {
+                if (splits[i].equals(mathOperator)) {
+                    if (index1 == -1) {
+                        index1 = i;
+                    } else if (index2 == -1 && i != index1) {
+                        index2 = i;
+                        break;
+                    }
+                }
+            }
+        }
+        if (index1 == -1 && index2 == -1) {
+            return null;
+        }
+        return new int[]{index1, index2};
+    }
+
+    private static String parseFirstNumber(String[] splits, int index) {
+        StringBuilder firstNumberString = new StringBuilder();
+        for (int i = 0; i < index; i++) {
+            firstNumberString.append(splits[i]);
+        }
+        return firstNumberString.toString();
+    }
+
+    private static String parseSecondNumber(String[] splits, int[] indexes) {
+        int start = indexes[0];
+        int end = indexes[1] != -1 ? indexes[1] : splits.length;
+        StringBuilder secondNumberString = new StringBuilder();
+        if (start + 1 == end) {
+            return null;
+        }
+        for (int i = start + 1; i < end; i++) {
+            secondNumberString.append(splits[i]);
+        }
+        return secondNumberString.toString();
+    }
+
+    private static void displayError(String message) {
+        System.out.println("Ошибка: " + message);
+    }
+
+    private static void printResult(String[] expression, double result) {
+        if (Double.isNaN(result)) {
+            return;
+        }
+        String resultString;
+        if (result == (int) result) {
+            resultString = String.valueOf((int) result);
+        } else {
+            String formattedResult = String.format("%.3f", result);
+            resultString = formattedResult.replaceAll("0+$", "");
+        }
+        System.out.println(expression[0] + " " + expression[1] + " " + expression[2] +
+                " = " + resultString);
     }
 }
--- src/com/startjava/lession2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lession2_3_4/calculator/CalculatorTest.java
@@ -5,26 +5,11 @@ import java.util.Scanner;
 public class CalculatorTest { 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Calculator calculator = new Calculator();
         String userChoose = "yes";
 
         while (userChoose.equals("yes")) {
             System.out.println("Введите выражение из 3 аргументов (например: 2 + 1)");
-            calculator.setFirstNumber(scanner.nextInt());
-            
-            while (true) {
-                System.out.print("Введите знак операции (+, -, *, /, ^, %): ");
-                char mathOperator = scanner.next().charAt(0);
-                if (calculator.setMathOperator(mathOperator)) {
-                    break;
-                }
-            }
-
-            System.out.print("Введите второе число: ");
-            calculator.setSecondNumber(scanner.nextInt());
-            calculator.calculate();
-            scanner.nextLine();
-
+            Calculator.calculate(scanner.nextLine());
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]:");
                 userChoose = scanner.nextLine().toLowerCase();
